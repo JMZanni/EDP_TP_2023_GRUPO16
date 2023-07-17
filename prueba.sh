b82ecf3ac0f9e196020c882a3a153039d6770419
JMZanni <juanmzanni@gmail.com>
2023-07-17 19:56:47 -0300

    Prueba de uso de expresión regular

--- prueba.sh
+++ prueba.sh
@@ -4,11 +4,25 @@
 carpeta="./imagenes"
 
 # Obtener la lista de nombres de archivos válidos
-archivos_validos=$(ls "$carpeta" | grep -E '^[A-Z][a-z]+ [A-Z][a-z]+$')
+#archivos_validos=$(ls "$carpeta" | grep -E '')
+
+#echo "$archivos_validos" > texto_pruebas.txt
 
 # Imprimir la lista de nombres de archivos válidos
-echo "Nombres de archivos válidos:"
-for archivo in $archivos_validos; do
+#echo "Nombres de archivos válidos:"
+#for archivo in $archivos_validos; do
 # ruta_archivo="$carpeta/$archivo"
-  echo "$archivo"
+#  echo "$archivo"
+#done
+
+for imagen in imagenes/*
+do
+	NOMBRE=$(basename "$imagen")
+	if [[ $NOMBRE =~ (^[A-Z][a-z]+ [A-Z][a-z]+$) ]]
+	then
+	echo $NOMBRE
+	#AQUI: PROCESAR Y GUARDAR EN /imagenes_procesadas
+	fi
 done
+
+exit 0
