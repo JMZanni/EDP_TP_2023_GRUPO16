e604a9bc16574885a9f75dea47e201dff83f9683
JMZanni <juanmzanni@gmail.com>
2023-07-18 20:44:27 -0300

    Comentamos el script procesar

--- procesar.sh
+++ procesar.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
+#Creamos la carpeta para guardar imagenes
+mkdir "imagenes_procesadas"
 
-if [ ! -d "imagenes_procesadas" ]
-then
-	mkdir -p "imagenes_procesadas"
-fi
-
+#Iteramos en la cantidad de imagenes de la carpeta "imagenes"
 for imagen in imagenes/*
 do
+	#Extraemos el nombre de cada imagen
 	NOMBRE=$(basename "$imagen")
+	#Procesamos solo si el nombre y apellido comienzan con mayuscula
 	if [[ $NOMBRE =~ (^[A-Z][a-z]+ [A-Z][a-z]+$) ]]
 	then
+	#Convertimos con ImageMagick y guardamos cada imagen en la carpeta imagenes procesadas con el mismo nombre
 	convert "imagenes/$NOMBRE" -gravity center -resize 512x512+0+0 -extent 512x512 "imagenes_procesadas/$NOMBRE" 
-	#AQUI: PROCESAR Y GUARDAR EN /imagenes_procesadas
 	fi
 done
 
